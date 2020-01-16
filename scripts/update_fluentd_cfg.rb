31bb8c7d20e4f1ebb4825d40684072c751c942b2
Brady Zuo <brady@zebrium.com>
2020-01-16 06:46:23 -0800

    Support list of file paths in "exclude" json config
    The "exclude" property now allows to specify multiple log file
    paths separated by commas.
    
    Also make log file mapping config file location "/etc/zebrium/log-file-map.cfg"
    obsolete, we now use "/etc/td-agent/log-file-map.conf" as preferred
    config file path. In the case that both config files exist, startup script
    would fail.

---
+++ scripts/update_fluentd_cfg.rb
@@ -0,0 +1,75 @@
+#!/opt/td-agent/embedded/bin/ruby
+
+require 'json'
+require 'logger'
+
+log = Logger.new(STDOUT)
+log.level = Logger::INFO
+
+file_map_cfg_file = "/etc/td-agent/log-file-map.conf"
+old_file_map_cfg_file = "/etc/zebrium/log-file-map.cfg"
+user_fluentd_cfg_file = "/etc/td-agent/conf.d/user.conf"
+
+if File.exist?(file_map_cfg_file) and File.exist?(old_file_map_cfg_file)
+    log.error("Both " + file_map_cfg_file + " and " + old_file_map_cfg_file + " exist")
+    exit(1)
+end
+
+if not File.exist?(file_map_cfg_file)
+    log.info(file_map_cfg_file + " does not exist, try " + old_file_map_cfg_file)
+    if not File.exist?(old_file_map_cfg_file)
+      exit(0)
+    end
+    log.warn(old_file_map_cfg_file + " is obsoleted, please move it to " + file_map_cfg_file)
+    file_map_cfg_file = old_file_map_cfg_file
+end
+
+log.info(file_map_cfg_file + " exist")
+file = File.read(file_map_cfg_file)
+file_mappings = JSON.parse(file)
+
+file_paths = ''
+exclude_paths = ''
+file_mappings['mappings'].each { |item|
+    if item.key?('file') and item['file'].length > 0
+        fpath = item['file']
+        if file_paths.length == 0
+            file_paths = fpath
+        else
+            file_paths = file_paths + "," + fpath
+        end
+    end
+    if item.key?('exclude') and item['exclude'].length > 0
+      exclude = item['exclude']
+      exclude.split(',').each {|str|
+        path = str.strip! || str
+        if exclude_paths.length == 0
+          exclude_paths = '"' + path + '"'
+        else
+          exclude_paths = exclude_paths + ',' + '"' + path + '"'
+        end
+      }
+    end
+}
+
+if file_paths.empty?
+    # File path must not be empty, Fluentd will fail if it is empty
+    file_paths = "/tmp/__dummy__.log"
+end
+user_cfg = '<source>
+  @type tail
+  path "FILE_PATHS"
+  exclude_path [EXCLUDE_PATHS]
+  path_key tailed_path
+  <parse>
+    @type none
+  </parse>
+  tag node.*
+</source>'
+
+user_cfg.sub!('FILE_PATHS', file_paths)
+user_cfg.sub!('EXCLUDE_PATHS', exclude_paths)
+
+open('/etc/td-agent/conf.d/user.conf', 'w') { |f|
+  f.puts user_cfg
+}
---
+++ scripts/zebrium_container_mon.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# This script monitors docker containers and create symbolic links to log
+# files of all running containers. This will help Fluentd to monitor all
+# container logs from single directory.
+
+. /opt/zebrium/etc/functions
+
+CONTAINER_LOG_DIR="/var/lib/zebrium/container_logs"
+
+update_log_links() {
+    local CONTAINER_IDS=`docker ps --no-trunc --format "{{.ID}}"`
+
+    mkdir -p $CONTAINER_LOG_DIR
+    cd $CONTAINER_LOG_DIR
+
+    for C in $CONTAINER_IDS; do
+        local LPATH=`docker inspect --format '{{.LogPath}}' $C`
+        local LINK=`basename $LPATH`
+        if [ -f $LINK ]; then
+            log INFO "Symbolic $LINK exists, skipping"
+        else
+            ln -s $LPATH .
+        fi
+    done
+
+    for F in `ls`; do
+        if [ ! -f $F ]; then
+            log INFO "Deleting invalid log file link $F"
+            rm -f $F
+        fi
+    done
+}
+
+main() {
+    log_start ""
+    while : ; do
+        update_log_links
+        sleep 30
+    done
+}
+
+main "$@"
