03e189a6df8848240e1743369faf392608d0936d
Brady Zuo <brady@zebrium.com>
2020-07-11 14:23:37 -0700

    Use Ubuntu 18.04 td-agent software on Ubuntu 20.04F

--- install_collector.sh
+++ install_collector.sh
@@ -322,8 +322,6 @@ function main() {
             log info "Ubuntu 20.04 (focal) detected, use compatible software from bionic."
             CODE_NAME=bionic
         fi
-            err_exit "Sorry, currently Ubuntu 20.04 (focal) is not supported."
-        fi
         if [ "$CODE_NAME" == "tricia" -o "$CODE_NAME" == "tina" -o "$CODE_NAME" == "tessa" -o "$CODE_NAME" == "tara" ]; then
             CODE_NAME="bionic"
         fi
